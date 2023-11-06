b4bbc84fcfa091e3a18e9a09845f4da6b86d6084
jadenl99 <100039013+jadenl99@users.noreply.github.com>
2023-11-06 16:20:02 -0500

    deterioration and distance analysis

--- src/custom_libraries/rate_analysis.py
+++ src/custom_libraries/rate_analysis.py
@@ -1,11 +1,8 @@
 import numpy as np
 import pandas as pd
-import sys
-import plotly.express as px
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
-import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix
+
+
+
 def nc_distance_from_damaged(
         df: pd.DataFrame, 
         start_year: int,
@@ -57,11 +54,21 @@ def nc_distance_vs_deterioration_rate(
         df: pd.DataFrame,
         distance_to_nearest_damaged_slab: np.ndarray,
         start_year: int,
-        end_year: int,
-        damaged_states: set = {'L1', 'T1', 'T2', 'L2', 'CC', 'SS', 'R'}
+        end_year: int
         ) -> pd.DataFrame:
-    
+    """Generates a table with the NC deterioration rate for each distance from a damaged slab, first column being distance, second column being deterioration rate.
+
+    Args:
+        df (pd.DataFrame): dataframe with all the slab state data
+        distance_to_nearest_damaged_slab (np.ndarray): a numpy array of the distance of each NC slab to its nearest damaged slab. Index 0 represents the first slab in the data, index 1 represents the second slab in the data, etc.
+        start_year (int): start year of the period of interest
+        end_year (int): end year of the period of interest
+
+    Returns:
+        pd.DataFrame: a dataframe with the first column being distance to nearest damaged slab, and the second column being the NC deterioration rate for that distance.
+    """
     # NC slabs that are still NC after 1 year are considered to have not deteriorated. An NC slab is considered to have deteriorated if it is NC in the first year and is either T1, L1, T2, L2, CC, SS, or R in the second year.
+    damaged_states = {'L1', 'T1', 'T2', 'L2', 'CC', 'SS', 'R'}
     conditions = [
         (df[str(start_year)] == 'NC') & ~(df[str(end_year)].isin(damaged_states)),
         df[str(start_year)] != 'NC'
@@ -86,7 +93,18 @@ def deteriorated_and_dist(
         damaged_states: set = {'L1', 'T1', 'T2', 'L2', 'CC', 'SS', 'R'}
 
         ) -> pd.DataFrame:
-    
+    """Generate a table with each NC slab's distance to its nearest damaged slab and whether or not it deteriorated. 0 represents not deteriorated, and 1 represents deteriorated.
+
+    Args:
+        df (pd.DataFrame): dataframe with all the slab state data
+        distance_to_nearest_damaged_slab (np.ndarray): a numpy array of the distance of each NC slab to its nearest damaged slab. Index 0 represents the first slab in the data, index 1 represents the second slab in the data, etc.
+        start_year (int): start year of the period of interest
+        end_year (int): end year of the period of interest
+        damaged_states (set, optional): Set of slab states to consider as damaged when marking whether an NC slab has deteriorated or not. Defaults to {'L1', 'T1', 'T2', 'L2', 'CC', 'SS', 'R'}.
+
+    Returns:
+        pd.DataFrame: a dataframe with each NC slab's distance to its nearest damaged slab and whether or not it deteriorated. 0 represents not deteriorated, and 1 represents deteriorated.
+    """
     # NC slabs that are still NC after 1 year are considered to have not deteriorated. An NC slab is considered to have deteriorated if it is NC in the first year and is either T1, L1, T2, L2, CC, SS, or R in the second year.
     
     conditions = [
@@ -102,14 +120,14 @@ def deteriorated_and_dist(
     new_df = new_df[new_df['initial'] == 'NC']
     return new_df
 
-def construct_nc_rate_table(
+def construct_nc_deterioration_rate_table(
         df: pd.DataFrame,
         start_year: int,
         end_year: int,
         interval: int, 
         damaged_states: set = {'L1', 'T1', 'T2', 'L2', 'CC', 'SS', 'R'}
         ) -> pd.DataFrame:
-    """Constructs a table with the NC rate for each distance from a damaged slab.
+    """Constructs a table with the NC deterioration rate for each distance from a damaged slab.
 
     Args:
         df (pd.DataFrame): dataframe with all the slab state data
@@ -122,17 +140,18 @@ def construct_nc_rate_table(
         pd.DataFrame: A table with the NC rate for each distance from a damaged slab for each interval year.
     """
     freq_by_year = {}
-    final_df = pd.DataFrame(columns=['distance', 'deterioration_rate'])
+    final_df = pd.DataFrame(columns=['distance', 'deterioration_rate', 'period'])
     first_end_interval = start_year + interval
     for e_year in range(first_end_interval, end_year + 1, interval):
         s_year = e_year - interval
-        nc_distance_from_damaged = nc_distance_from_damaged(df, s_year, damaged_states)
-        curr_interval_data = nc_distance_vs_deterioration_rate(df, nc_distance_from_damaged, s_year, end_year, damaged_states)
+        nc_distance_damaged = nc_distance_from_damaged(df, s_year, damaged_states)
+        curr_interval_data = nc_distance_vs_deterioration_rate(df, nc_distance_damaged, s_year, e_year)
         curr_interval_data = curr_interval_data.div(curr_interval_data.sum(axis=1), axis=0)
         curr_interval_data = curr_interval_data.drop(0, axis=1)
         curr_interval_data['distance'] = curr_interval_data.index
+        curr_interval_data['period'] = f'{s_year}-{e_year}'
         curr_interval_data = curr_interval_data.rename(columns={1 : 'deterioration_rate'})
         
-        final_df = pd.concat(final_df, curr_interval_data, axis=0)
+        final_df = pd.concat([final_df, curr_interval_data], axis=0)
     
     return final_df
